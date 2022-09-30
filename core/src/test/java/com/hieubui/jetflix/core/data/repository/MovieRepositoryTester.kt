b7133e418e61520174f7ad889973bb605ee92f36
hieubui <hieubui00.it@gmail.com>
2022-09-30 11:18:00 +0700

    Integrate Unit Test for Movie Repository in Core module

---
+++ core/src/test/java/com/hieubui/jetflix/core/data/repository/MovieRepositoryTester.kt
@@ -0,0 +1,71 @@
+package com.hieubui.jetflix.core.data.repository
+
+import com.google.gson.Gson
+import com.hieubui.jetflix.core.data.remote.request.TheMovieDatabaseService
+import com.hieubui.jetflix.core.data.remote.response.MoviesResponse
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+internal class MovieRepositoryTester {
+    @MockK
+    private lateinit var theMovieDatabaseService: TheMovieDatabaseService
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this@MovieRepositoryTester)
+    }
+
+    private fun setupMovieRepository(scheduler: TestCoroutineScheduler): MovieRepository {
+        val dispatcher = StandardTestDispatcher(scheduler)
+
+        return MovieRepositoryImpl(theMovieDatabaseService, dispatcher)
+    }
+
+    @Test
+    fun `When page less than 1 then return empty list`() = runTest {
+        val movieRepository = setupMovieRepository(testScheduler)
+        val testCases = -5..0
+        val result = testCases.map { movieRepository.getDiscoverMovies(it) }
+
+        assert(result.all { it.isEmpty() })
+    }
+
+    @Test
+    fun `When page greater than 500 then return empty list`() = runTest {
+        val movieRepository = setupMovieRepository(testScheduler)
+        val testCases = 501..505
+        val result = testCases.map { movieRepository.getDiscoverMovies(it) }
+
+        assert(result.all { it.isEmpty() })
+    }
+
+    @Test
+    fun `When page greater than 0 and less than 500 then return a data list`() = runTest {
+        val movieRepository = setupMovieRepository(testScheduler)
+        val response = mockResponse<MoviesResponse>("discover_movies_response.json")
+
+        coEvery { theMovieDatabaseService.getDiscoverMovies(any()) }.returns(response)
+
+        val testCases = (1..5) + (495..500)
+        val result = testCases.map { movieRepository.getDiscoverMovies(it) }
+
+        assert(result.all { it.size == response.results?.size })
+    }
+
+    private inline fun <reified T> mockResponse(fileName: String): T {
+        val responseJSON = javaClass.classLoader
+            ?.getResourceAsStream(fileName)
+            ?.bufferedReader()
+            ?.use { it.readText() }
+
+        return Gson().fromJson(responseJSON, T::class.java)
+    }
+}
